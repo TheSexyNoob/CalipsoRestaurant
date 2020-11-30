162284620ba48720c6e57116cccc3c914f36aeb4
Luis Alberto Nuñez Reyes <lanr@hotmail.es>
2020-11-29 21:58:10 -0600

    Menu View

---
+++ CalipsoRestaurant/CalipsoRestaurant/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using CalipsoRestaurant.ViewModels;
+using CalipsoRestaurant.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace CalipsoRestaurant
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+      
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+          
+
+        }
+    }
+}
---
+++ CalipsoRestaurant/CalipsoRestaurant/Views/MenuPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CalipsoRestaurant.Views
+{
+    public partial class MenuPage : ContentPage
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
