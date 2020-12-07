dd05a96d1fe21808f34abddda3496d8f99d814e5
Luis Nunez Reyes <lanr@hotmail.es>
2020-12-07 01:17:03 -0600

    DETALLE VIEW MODEL VISTAS DE PABLO

---
+++ CalipsoRestaurant/CalipsoRestaurant/ViewModels/DetalleOrdenViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace CalipsoRestaurant.ViewModels
+{
+    class DetalleOrdenViewModel : BaseViewModel
+    {
+        public DetalleOrdenViewModel()
+        {
+            Title = "Detalle Orden";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
