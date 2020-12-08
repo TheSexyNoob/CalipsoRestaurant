92669357180a17852dad9c0f3614545de90012a7
Luis Nunez Reyes <lanr@hotmail.es>
2020-12-07 21:53:52 -0600

    detalle platillo view model

---
+++ CalipsoRestaurant/CalipsoRestaurant/ViewModels/DetallePlatilloViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace CalipsoRestaurant.ViewModels
+{
+    class DetallePlatilloViewModel : BaseViewModel
+    {
+        public DetallePlatilloViewModel()
+        {
+            Title = "Detalle Platillo";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
